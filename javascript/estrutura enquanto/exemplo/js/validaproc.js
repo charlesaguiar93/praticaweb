1134ea0080f842ffc9c0f22730f53f6034ba22f5
charlesaguiar93 <charlesaguiar93@gmail.com>
2025-08-11 20:14:55 -0300

    atividade

---
+++ javascript/estrutura enquanto/exemplo/js/validaproc.js
@@ -0,0 +1,13 @@
+function validaproc(){
+
+
+    let i=1;
+    while(i<=10){
+        // Aqui você pode adicionar a lógica que deseja executar em cada iteração
+
+        console.log("Passando na estrutura a "+i+"ª vez o valor de i é "+i);
+        i++;
+    }
+    return false;
+
+}
