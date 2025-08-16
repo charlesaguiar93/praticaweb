0d1b884085b6cb4bb54dbc45c1e443dde91c7f8f
charlesaguiar93 <charlesaguiar93@gmail.com>
2025-08-16 11:04:33 -0300

    atividade3

--- javascript/vetorunidimencional/atividade3/js/validaproc.js
+++ javascript/vetorunidimencional/atividade3/js/validaproc.js
@@ -2,30 +2,26 @@ function validaProc(){
 
 let i;
 
-let podicao={}
+let numero={}
 
-for(i=1;i<=10;i++){
-podicao[i]=document.getElementById("podicao"+i);
-console.log("podicao"+i+": "+podicao[i]);
+for(i=0;i <10; i++){
+    numero[i]=i*i;// Calcula o quadrado do número i
+    
+    // Exibe o valor do quadrado na posição i do vetor
+    console.log("Posição "+i+" - Valor: "+numero[i]);
+    
+
 
 }
 
 
-/*Atividade2
-faça um programa para ler as notas obtidas pelos alunos, e depois exibir um relatório
-das notas iguais ou superior a 7,5 e também 
-no final a quantidade de notas iguais ou superior a 7,5
 
-atividade3
-faça um programa para gerar um vetor de 10 posições, onde cada elemento corresponde ao 
-quadrado de sua posição. Imprima depois os dados armazenados no vetor.  
-
-
-
+return false
 
 
 
 }
+
 
 
 
