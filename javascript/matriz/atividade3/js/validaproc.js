05c99b97394c4e50e78e5cd1f509e98c58c21e09
charlesaguiar93 <charlesaguiar93@gmail.com>
2025-08-20 17:41:16 -0300

    matriz

--- javascript/matriz/atividade3/js/validaproc.js
+++ javascript/matriz/atividade3/js/validaproc.js
@@ -4,31 +4,39 @@ function validaProc(){
 let matriz=[];
 let linhas = 2;
 let colunas = 2;
+let linha;
 let soma = 0;
+let posicao = false;
 
-for(i=0; i < linhas; i++){
+
+for(i=0; i < linhas; i++ ){
 
     matriz[i]=[]
-    for(c=0; c < colunas;){
-       
-        matriz[i][c]= parseInt(prompt("Digite o valor da linha " + i + "º coluna"+c));
-     
-    }
-}
- 
-/*for(i=0; i< linhas; i++){
-
     for(c=0; c < colunas; c++){
 
-    soma = soma + matriz[i][c];
-          
+        matriz[i][c] = parseInt(prompt("Digite o "+(i+1)+"º número da linha e da "+(c+1)+"ª coluna:"));
+    }
 
-        console.log("Posição "+i+" - Valor: "+soma);
 
     }
-}*/
+
+ 
+    for(i=0; i < linhas; i++){
+    
+        for(c=0; c < colunas; c++){
+
+    soma = matriz[i][c] + matriz[i][c]     
+       document.write(soma+"&nbsp&nbsp");
+}
+document.write("<br>");  
+    console.log("A soma da linha[",linha,"] é :"+soma);
+}
+
+
+
 
 return false;
+
 
 
 
