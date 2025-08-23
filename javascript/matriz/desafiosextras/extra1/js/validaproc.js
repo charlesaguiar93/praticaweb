de27cc848d3647635ec02b7b116d9a67faa459c6
charlesaguiar93 <charlesaguiar93@gmail.com>
2025-08-23 12:58:27 -0300

    extra1

--- javascript/matriz/desafiosextras/extra1/js/validaproc.js
+++ javascript/matriz/desafiosextras/extra1/js/validaproc.js
@@ -10,51 +10,30 @@ for(i=0; i < linhas; i++ ){
     for(c=0; c < colunas; c++){
 
         matriz[i][c] = parseInt(prompt("Digite o número da "+(i+1)+"ª linha e "+(c+1)+"ª coluna:"));
-    
+        document.write(matriz[i][c]+"&nbsp&nbsp"); 
        
-        
-        //console.log("Linha é"+(i+1)+"ª linha e"+(c+1)+"ª coluna"+ matriz[i][c]);
-        
-        
-    
-    }
-    
+        }
+     document.write("<br>");
 }
 
-for(i=0; i < linhas; i++){
-console.log(matriz[i][i],"|")
-document.write(matriz[i][i]+"&nbsp&nbsp"); 
-document.write("<br>");
-} 
-    for(i=0; i < linhas; c++){
 
-        
-        console.log(matriz[i][3 - 1],"|")
-        document.write(matriz[i][3 - 1]+"&nbsp&nbsp"); 
-        document.write("<br>");
+for(i=0; i < linhas; i++){
+
+   console.log(matriz[i][i]+" | ")
+   document.write(matriz[i][i]+"&nbsp&nbsp"); 
+   document.write("<br>");
+} 
     
+       
+    for(i=0; i < linhas; i++){
+       
+       console.log(matriz[i][3 - i]+" | ")
+       document.write(matriz[i][3 - i]+"&nbsp&nbsp"); 
+       // document.write("<br>");
+        
     }        
    
 
 return false;
-
-
-
-
-
-
-//console.log(prompt("A linha:[" +i+ "] coluna:[" +c+"] :"+matriz[i][i]));
-    //console.log(prompt("A linha:[" +i+ "] [" +c+"] :"+matriz[i][3 - i]));
-    
-
-
-
-
-
-
-
-
-
-
 
 }
